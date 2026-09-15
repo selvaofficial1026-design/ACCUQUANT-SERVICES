9fe665787b8b8761ade66e2e7cb1c8b81bfcd389
selvaofficial1026-design <selvaofficial1026@gmail.com>
2026-09-15 20:44:12 +0530

    Add start-dev.bat launcher

---
+++ start-dev.bat
@@ -0,0 +1,8 @@
+@echo off
+title ACCUQUANT SERVICES - Vite Dev Server
+cd /d "D:\ACCUQUANT SERVICES"
+echo ===================================================
+echo   Starting ACCUQUANT SERVICES Development Server...
+echo ===================================================
+npm run dev
+pause
